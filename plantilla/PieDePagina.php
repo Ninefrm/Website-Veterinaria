1c59401050e93d23c32a76d0207f68d5181b94b7
Maximiliano Fonseca <maxfonseca.r@gmail.com>
2019-11-07 08:01:52 -0600

    Veterinaria - Comienzo

---
+++ plantilla/PieDePagina.php
@@ -0,0 +1,18 @@
+<footer class="page-footer">
+    <div class="footer-copyright">
+        <div class="container">
+            <a target="_blank": href="http://www.facebook.com/MAXIMILIANOFRM" title="NINEFRM">Veterinaria</a>
+            <p>Fonseca Romero, Samuel Maximiliano.</p>
+
+            <a class="grey-text text-lighten-4 right" href="#!"><p>SESIÓN: <?php
+                    if (!empty($_SESSION['id'])){
+                        print_r($_SESSION['nombre']);
+                    }
+                    else{
+                        echo "SIN SESIÓN ACTIVA";
+                    }
+                    ?></p></a>
+
+        </div>
+    </div>
+</footer>
